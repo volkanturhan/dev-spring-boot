d2b1d71d9751cc32b6675a494844b00b0c97e5d9
Volkan TURHAN <volkanturhan@msn.com>
2023-10-06 06:21:27 +0300

    first commit

---
+++ 01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/volkanturhan/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,18 @@
+package com.volkanturhan.springboot.demo.mycoolapp.rest;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @GetMapping("/*")
+    public String sayHello() {
+        return "Hello World!lkmjlkj";
+    }
+}
---
+++ 01-spring-boot-overview/06-properties-demo/src/main/java/com/volkanturhan/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,37 @@
+package com.volkanturhan.springboot.demo.mycoolapp.rest;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
+    // expose new endpoint for "teaminfo"
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return  "Coach : " + coachName + ", Team Name : "+teamName;
+    }
+
+    @GetMapping("/*")
+    public String sayHello() {
+        return "Hello World!";
+    }
+    //expose a new endpoint for "workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+    //expose a new endpoint for "fortune"
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is lucky day.";
+    }
+}
